9fd703e02c9629696ddd6e7c078403380fb42f22
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2024-10-10 16:16:43 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ToDoList from './components/ToDoList.vue';
+
+
+</script>
+
+<template>
+  <ToDoList/>
+</template>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+ 
+}
+</style>
